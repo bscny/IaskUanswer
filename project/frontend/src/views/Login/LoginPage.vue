27a703570ea3acaaf1cd816f2e4ab88a4421abdb
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2024-12-18 16:57:26 +0800

    [Create] add login/signup page in src/view, and add them in router.

---
+++ project/frontend/src/views/Login/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div>
+        <NavBar/>
+    </div>
+    <div>
+        
+    </div>
+
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default{
+
+    name: 'LoginPage',
+    components:{
+        NavBar
+    }
+}
+</script>
