2e1e336e97fda3d4fca8b31d671866e981317178
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2024-12-18 15:32:06 +0800

    [Create] add homepage in src/views/ and router configuration

--- project/frontend/src/router/index.js
+++ project/frontend/src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UserLibrary from "@/views/UserLibraryPages/UserLibrary.vue"
+import HomePage from '@/views/Home/HomePage.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: '/',
+            component: HomePage
+        },
         {
             path: '/UserLibrary',
             name: 'UserLibrary',
