330096dcef029f2f0fed635dc4af0356fa1abb4d
nonohuang <yh7624908@email.com>
2024-12-19 20:18:44 +0800

    [Test] it is a test

--- project/backend/src/db_services/testFunction/testServices.js
+++ project/backend/src/db_services/testFunction/testServices.js
@@ -3,8 +3,8 @@ const db = require('@/database.js');
 // read services
 async function GetTest(Id) {
     const [records] = await db.query(`select *
-                                      from test
-                                      where Id = ?`, [Id]);
+                                      from user
+                                      `);
 
     return records;
 }
