715f3deea1ecaccec47afe2951097f5f53f437b8
jingxiang0405 <shane200445@gmail.com>
2024-12-22 15:58:10 +0800

    [Create] implement QuizGrid that shows 9 quizzes from database.

--- project/frontend/src/service/Api.js
+++ project/frontend/src/service/Api.js
@@ -11,6 +11,10 @@ export default {
 
     },
 
+    async getFirstNQuizes(n) {
+        return axios.get("/", { params: { nQuiz: n } });
+    },
+
     // for testing
     async fake_login(data) {
         const fakeData = {
@@ -38,6 +42,71 @@ export default {
             password: data.password,
             username: data.username
         };
+
+        const fakeAxiosResponse = {
+            data: fakeData,
+            status: 200,
+            statusText: 'OK',
+        };
+
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(fakeAxiosResponse), 500); // Simulate async delay
+        });
+    },
+
+    async fake_getNQuizes(n) {
+        const fakeData = {
+            quizzes: [
+                {
+                    "id": 1,
+                    "title": "Tech Titans Trivia",
+                    "description": "Test your knowledge of the world's most influential tech companies, innovations, and industry leaders."
+                },
+                {
+                    "id": 2,
+                    "title": "World Wonders Quest",
+                    "description": "Explore the marvels of the world through a quiz that takes you across continents, cultures, and iconic landmarks."
+                },
+                {
+                    "id": 3,
+                    "title": "Brain Buster Bonanza",
+                    "description": "Challenge your mind with puzzles, riddles, and brain teasers designed to twist your thinking."
+                },
+                {
+                    "id": 4,
+                    "title": "Pop Culture Powerhouse",
+                    "description": "Are you the ultimate fan? Dive into the worlds of music, movies, TV, and internet trends."
+                },
+                {
+                    "id": 5,
+                    "title": "History Highlights Hunt",
+                    "description": "Journey through time with questions about epic events, famous figures, and intriguing historical trivia."
+                },
+                {
+                    "id": 6,
+                    "title": "Science Spectrum Showdown",
+                    "description": "From the depths of the ocean to the farthest stars, test your scientific curiosity and knowledge."
+                },
+
+                {
+
+                    "id": 7,
+                    "title": "Game On! Gaming Quiz",
+                    "description": "A must-play for gamers! Explore trivia from retro classics to cutting-edge titles."
+                },
+                {
+                    "id": 8,
+                    "title": "Foodie Frenzy",
+                    "description": "How much do you know about global cuisines, culinary techniques, and mouth-watering dishes?"
+                },
+                {
+                    "id": 9,
+                    "title": "Sports Saga Challenge",
+                    "description": "Prove your prowess in sports knowledge, from legendary athletes to iconic moments in sports history."
+                }
+            ]
+
+        }
 
         const fakeAxiosResponse = {
             data: fakeData,
--- project/frontend/src/views/Home/HomePage.vue
+++ project/frontend/src/views/Home/HomePage.vue
@@ -1,14 +1,18 @@
 <template>
-    <NavBar/>
-    <SideBar/>
+    <NavBar />
+    <div style="display: flex;">
+        <SideBar />
+        <QuizGrid />
+    </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
 import SideBar from '@/components/Home/SideBar.vue';
-export default{
-    components:{
-        NavBar, SideBar
+import QuizGrid from '@/components/Home/QuizGrid.vue';
+export default {
+    components: {
+        NavBar, SideBar, QuizGrid
     }
 }
 </script>
